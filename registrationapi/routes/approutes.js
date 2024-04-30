c4aa9081c9fb0daa810b0b366e7911f9ea20fe3c
Parameswari Bala <parameswaribala@gmail.com>
2024-04-30 14:57:45 +0530

    updated

--- registrationapi/routes/approutes.js
+++ registrationapi/routes/approutes.js
@@ -3,6 +3,46 @@ const router = express.Router();
 const userController= require('../controllers/usercontroller')
 const roleController=require('../controllers/rolecontroller')
 //save user path
+/**
+ * @swagger
+ * /api/users/v1.0/:
+ *  post:
+ *    description: Use to add user in DB
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - in: body
+ *        name: Add user
+ *        description: Add user in DB.
+ *        schema:
+ *          type: object
+ *          required:
+ *            - firstName
+ *            - lastName
+ *            - dob
+ *            - gender
+ *            - mobileNo
+ *            - roles
+ *          properties:
+ *            firstName:
+ *              type: string
+ *            lastName:
+ *              type: string
+ *            dob:
+ *              type: string
+ *            gender:
+ *              type: string
+ *            mobileNo :
+ *              type: integer
+ *            roles:
+ *              type: array
+ *              items:
+ *                  type: string
+ *
+ *    responses:
+ *      '200':
+ *        description: User added successfully.
+ */
 router.post('/users/v1.0/', userController.saveUser)
 //save role path
 router.post('/roles/v1.0/',roleController.saveRole)
