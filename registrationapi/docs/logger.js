0f904124ff207161a626bda470888324e44fb6b0
Parameswari Bala <parameswaribala@gmail.com>
2024-05-03 11:03:57 +0530

    updated

--- registrationapi/docs/logger.js
+++ registrationapi/docs/logger.js
@@ -2,12 +2,19 @@
  * Configurations of logger.
  */
 const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+const { format, level, prettyPrint } = require('winston');
 
+const DailyRotateFile = require('winston-daily-rotate-file');
+const timezoned = () => {
+    return new Date().toLocaleString('en-US', {
+        timeZone: 'Asia/Mumbai'
+    });
+}
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(),
+        //winston.format.timestamp(),
+        winston.format.combine(format.timestamp({ format: timezoned }),format.prettyPrint()),
         winston.format.printf(({ timestamp, level, message }) => {
             return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
         })
@@ -15,7 +22,7 @@ const logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
         new DailyRotateFile({
-            filename: 'logs/application-%DATE%.log',
+            filename: 'logs/registration-%DATE%.log',
             datePattern: 'YYYY-MM-DD',
             zippedArchive: true,
             maxSize: '20m',
