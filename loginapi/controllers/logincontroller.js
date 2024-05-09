2ab8e7228a10e7310c8ba79727c61ff50c558eb5
Parameswari Bala <parameswaribala@gmail.com>
2024-05-09 14:58:20 +0530

    updated

--- loginapi/controllers/logincontroller.js
+++ loginapi/controllers/logincontroller.js
@@ -10,16 +10,9 @@ exports.validateUser=async(req,res)=>{
     })
         .then(function (response) {
             console.log(response);
-            res.status(config.get('statusCode.success')).send({
-
-                user:response
         })
         .catch(function (error) {
             console.log(error);
-            res.status(config.get('statusCode.logicError')).send({
-                message:`user could not be found for the given mobileNo ${mobileNo}`,
-                errorMessage: error.message
-            })
         });
 
 }
