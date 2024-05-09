e065086d45358d422e91c9ce31c2076d55aab8db
Parameswari Bala <parameswaribala@gmail.com>
2024-05-09 16:21:42 +0530

    updated

--- loginapi/services/valudateusergl.js
+++ loginapi/services/valudateusergl.js
@@ -1,39 +1,43 @@
 const http=require('http')
 const config=require('config')
-const data = JSON.stringify({
-    query: `{
+exports.validateUserGL=()=>{
+
+    const data = JSON.stringify({
+        query: `{
      validateUser(firstName: ${config.get(graphqlData.firstName)}, mobileNo: ${config.get(graphqlData.mobileNo)}) {
         dob
         lastName
     }
   }`,
-});
-const options = {
-    hostname: 'localhost',
-    path: 'graphql',
-    port: 3200,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        'User-Agent': 'Node',
-    },
-};
-const req = http.request(options, (res) => {
-    let data = '';
-    console.log(`statusCode: ${res.statusCode}`);
-
-    res.on('data', (d) => {
-        data += d;
     });
-    res.on('end', () => {
-        console.log(JSON.parse(data).data);
-    });
-});
-req.on('error', (error) => {
-    console.error(error);
-});
+    const options = {
+        hostname: 'localhost',
+        path: 'graphql',
+        port: 3200,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'User-Agent': 'Node',
+        },
+    };
+    const req = http.request(options, (res) => {
+        let data = '';
+        console.log(`statusCode: ${res.statusCode}`);
 
-req.write(data);
-req.end();
+        res.on('data', (d) => {
+            data += d;
+        });
+        res.on('end', () => {
+            console.log(JSON.parse(data).data);
+        });
+    });
+    req.on('error', (error) => {
+        console.error(error);
+    });
+
+    req.write(data);
+    req.end();
+}
+
 
