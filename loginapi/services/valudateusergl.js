cc74702cf4b8625fe7609aa20a082b43b700fff3
Parameswari Bala <parameswaribala@gmail.com>
2024-05-09 16:13:52 +0530

    updated

---
+++ loginapi/services/valudateusergl.js
@@ -0,0 +1,39 @@
+const http=require('http')
+
+const data = JSON.stringify({
+    query: `{
+     validateUser(firstName: "Balasubramaniam", mobileNo: "8056050425") {
+        dob
+        lastName
+    }
+  }`,
+});
+const options = {
+    hostname: 'localhost',
+    path: 'graphql',
+    port: 3200,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+        'User-Agent': 'Node',
+    },
+};
+const req = http.request(options, (res) => {
+    let data = '';
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', (d) => {
+        data += d;
+    });
+    res.on('end', () => {
+        console.log(JSON.parse(data).data);
+    });
+});
+req.on('error', (error) => {
+    console.error(error);
+});
+
+req.write(data);
+req.end();
+
