67a0d20f107eedffcbe9cab1e69cf23125763248
Parameswari Bala <parameswaribala@gmail.com>
2024-05-14 10:27:30 +0530

    updated

--- chitapi/controllers/chitcontroller.js
+++ chitapi/controllers/chitcontroller.js
@@ -1,13 +1,14 @@
 const config=require('config')
+axios = require("axios");
 exports.createChit=async(req,res)=>{
     const token=req.header('Authorization').split(' ')[1];
     const validationUrl=config.get('services.validationUrl');
     axios.post(validationUrl,{
         "token":token
     }).then(response=>{
-        console.log(response);
+        console.log(response.data);
         res.status(200).send({
-            "receivedToken":response
+            "receivedToken":response.data.receivedToken
         })
     }).catch(error=>{
         console.log(error);
