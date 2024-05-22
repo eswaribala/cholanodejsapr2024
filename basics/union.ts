8dbe12a4083b34dc574fa6aa884655c076d315b4
Parameswari Bala <parameswaribala@gmail.com>
2024-05-22 10:41:28 +0530

    updated

---
+++ basics/union.ts
@@ -0,0 +1,43 @@
+import {Individual} from "../models/Individual";
+import {AutoGenerator} from "../models/autogenerator";
+import {Name} from "../models/name";
+import {Address} from "../models/address";
+import {Gender} from "../models/gender";
+import {Corporate} from "../models/corporate";
+import {CompanyType} from "../models/companytype";
+import {SavingsAccount} from "../models/savingsaccount";
+
+let value:number=Math.floor(Math.random()*3);
+console.log(`The Generated Value=${value}`)
+
+type customType='Individual'|'Corporate'|'Account'
+
+let choices:[customType,customType,customType]=['Individual','Corporate','Account']
+let individual:Individual=new Individual(AutoGenerator.generateAccountNo(10000000,1000),new Name("Parameswari",
+        "Bala",""),new Address("10d","First Street",
+        "Avadi","TN"),9952032862,"param@gmail.com","Test@123",
+    Gender.FEMALE,new Date(1970,12,2))
+console.log(individual)
+let guess=(choice:customType):object=>{
+    let object:Object=null;
+    switch (choice){
+        case 'Individual':
+            object=individual
+            break
+        case 'Corporate':
+            object=new Corporate(AutoGenerator.generateAccountNo(10000000,1000),new Name("VHEBANDCO",
+                    "Software Consulting Services",""),new Address("10d","First Street",
+                    "Avadi","TN"),9952032863,"vhebcompany@gmail.com","Test@123",
+                CompanyType.PRIVATE);
+            break
+        case 'Account':
+            object=new SavingsAccount(2848732,
+                new Date(AutoGenerator.generateYear(2024,1970),
+                    12,1),individual,0.8)
+            break
+
+    }
+    return object;
+}
+
+guess(choices[value])
