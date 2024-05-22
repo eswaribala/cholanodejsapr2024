5f2c1801c0e00db689e44b2b93451a0b379d6e9c
Parameswari Bala <parameswaribala@gmail.com>
2024-05-22 11:53:59 +0530

    updated

--- basics/asyncawait.ts
+++ basics/asyncawait.ts
@@ -4,11 +4,26 @@ let fetchUsers=async():Promise<any|string>=>{
 
      let response:any|string=null;
     try {
-       response = await fetch(api)
+       let data = await fetch(api)
+        response=await data.json()
+        return response;
     }catch(error){
        response=error;
+       return response;
     }
-return response
+
 }
 
-console.log(fetchUsers());
+//invoke async function
+// @ts-ignore
+async function executeAsync():Promise<void> {
+    try {
+        let response = await fetchUsers();
+        console.log(response);
+    } catch (err) {
+        console.log("Error is " + err);
+    }
+}
+console.log("Before calling a function");
+executeAsync();
+console.log("After calling a function");
