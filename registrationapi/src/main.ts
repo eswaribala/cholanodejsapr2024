9df3c93d7397a6ca8b102be39490d46108d4471d
Parameswari Bala <parameswaribala@gmail.com>
2024-05-22 15:45:56 +0530

    updated

--- registrationapi/src/main.ts
+++ registrationapi/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+      .setTitle('Customer API')
+      .setDescription('The CustomerAPI description')
+      .setVersion('1.0')
+      .addTag('customers')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  //http://localhost:3000/api
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
